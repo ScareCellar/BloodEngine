ace0b54f735f2d3d73bc9768f8e69983029bbac4
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2025-08-24 20:18:46 -0600

    removed Json test code

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -14,39 +14,6 @@ using namespace blood;
 int main(int argc, char* argv[]) { 
     blood::file::SetCurrentDirectory("Assets");
 
-    // load the json data from a file
-    std::string buffer;
-    blood::file::ReadTextFile("json.txt", buffer);
-    // show the contents of the json file (debug)
-    std::cout << buffer << std::endl;
-
-    // create json document from the json file contents
-    rapidjson::Document document;
-    blood::json::Load("json.txt", document);
-
-    // read/show the data from the json file
-    std::string name;
-    int age;
-    float speed;
-    bool isAwake;
-    blood::vec2 position;
-    blood::vec3 color;
-
-    // read the json data
-    JSON_READ(document, name);
-    JSON_READ(document, age);
-    JSON_READ(document, speed);
-    JSON_READ(document, isAwake);
-    JSON_READ(document, position);
-    JSON_READ(document, color);
-
-    // show the data
-    std::cout << name << " " << age << " " << speed << " " << isAwake << std::endl;
-    std::cout << position.x << " " << position.y << std::endl;
-    std::cout << color.r << " " << color.g << " " << color.b << " " << std::endl;
-
-    return 0;
-
     std::cout << argc << std::endl;
 
     for (int i = 0; i < argc; i++) {
